cc516ed63cc99e504ad5bc1d7b323a34ffb572bd
Yulianto <yuliantoardin@gmail.com>
2023-12-09 16:15:41 +0700

    init

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <>
+      <h1>Page Under Construction</h1>
+    </>
+  );
+};
+
+export default NotFound;
---
+++ src/routes/index.ts
@@ -0,0 +1,22 @@
+import { lazy } from 'react';
+
+const Chart = lazy(() => import('../pages/Chart'));
+const NotFound = lazy(() => import('../pages/NotFound'));
+
+
+const coreRoutes = [
+  {
+    path: '/chart',
+    title: 'Chart',
+    component: Chart,
+  },
+  {
+    path: '/NotFound',
+    title: 'NotFound',
+    component: NotFound,
+  },
+
+];
+
+const routes = [...coreRoutes];
+export default routes;
